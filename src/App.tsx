baf2d9465955bf8af9affb39bbd59bd855f36614
Luke Fredrickson <lukeefredrickson@gmail.com>
2022-11-14 08:32:36 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CurrentWeather } from "./apiResponseTypes";
+import { API_KEY } from "./config";
+import "./App.css";
+
+// burlington, vt coords
+const LAT = 44.4759;
+const LON = -73.2121;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [lat, setLat] = useState<number>(LAT);
+  let [lon, setLon] = useState<number>(LON);
+  let [data, setData] = useState<CurrentWeather | undefined>(undefined);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
+        { mode: "cors" }
+      );
+      let data: CurrentWeather = await response.json();
+      console.log(data);
+      setData(data);
+    }
+    fetchData();
+  }, [lat, lon]);
+
+  let weather =
+    data === undefined ? "" : <pre>{JSON.stringify(data, undefined, 2)}</pre>;
+
+  return <div className="App">{weather}</div>;
 }
 
 export default App;
